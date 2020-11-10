994a4146e63244865b3a465c84a6a641f135a741
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-11-10 22:23:49 +0200

    Basic Users controller

---
+++ API-Role-Based-Authorization/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Role_Based_Authorization.Entities;
+using API_Role_Based_Authorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API_Role_Based_Authorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest request)
+        {
+            return Ok();
+        }
+
+        // [Authorize(Roles = Role.Admin)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
